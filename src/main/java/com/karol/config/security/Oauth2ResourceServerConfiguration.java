71755159cdaae6ebb22e945082d42a60cddb3298
KarolWojtas <karol.wojtas.aim@gmail.com>
2018-09-22 13:15:50 +0200

    fabric8

--- src/main/java/com/karol/config/security/Oauth2ResourceServerConfiguration.java
+++ src/main/java/com/karol/config/security/Oauth2ResourceServerConfiguration.java
@@ -1,8 +1,8 @@
 package com.karol.config.security;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,6 +12,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.expression.OAuth2WebSecurityExpressionHandler;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 
 
@@ -26,16 +30,28 @@ public class Oauth2ResourceServerConfiguration extends ResourceServerConfigurerA
 	private AuthenticationManager authManager;
 	@Autowired
 	private OAuth2WebSecurityExpressionHandler oauth2WebSecurityExpressionHandler;
+	@Value("${allowed-origin}")
+	private String allowedOrigin;
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+			.antMatchers("/content/**").permitAll()
+            .antMatchers("/content/venues").permitAll()
+			.antMatchers(HttpMethod.GET, "/content/venues/{venueId}/avatar").permitAll()
+			.antMatchers(HttpMethod.POST, "/content/venues/{venueId}/avatar").hasRole("ADMIN")
+			.antMatchers("/content/venues/{venueId}").hasRole("ADMIN")
 			.antMatchers("/users/auth/**").denyAll()
-			.antMatchers(HttpMethod.GET, "/users/{username}").permitAll()
+			.antMatchers(HttpMethod.GET, "/users/{username}","/users/checkusername/{username}","/users/{username}/avatar").permitAll()
+			.antMatchers(HttpMethod.POST,"/users/").permitAll()
 			.antMatchers("/users/{username}","/users/{username}/**")
 				.access("@pathAccessService.usernameEqualsPrincipal(authentication,#username)")
-			.antMatchers("/uaa/oauth/token").permitAll()
+			.antMatchers(HttpMethod.PATCH, "/users/{username}")
+				.access("@pathAccessService.usernameEqualsPrincipal(authentication,#username)")
+			.antMatchers("/uaa/oauth/token**").permitAll()
 			.antMatchers(HttpMethod.GET, "/social/**").permitAll()
 			.antMatchers("/**").authenticated()
+			.and()
+			.cors()
 			;
 	}
 	
@@ -47,6 +63,24 @@ public class Oauth2ResourceServerConfiguration extends ResourceServerConfigurerA
 			.authenticationManager(authManager);
 		
 	}
+	@Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new     UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin(allowedOrigin);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+	
 	
 	
 }
